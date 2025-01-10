3cf343c0fb523e6b8edeeb6624f0268afc386969
gt-yordanos <gt.yordanos@gmail.com>
2025-01-10 16:14:22 +0300

    craeted the navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import '../Styles/components/Navbar.css'
+import littleLemonLogo  from '../assets/littleLemonLogo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="nav-container">
+      <div className="humbergur">
+        <GiHamburgerMenu size={24} style={{ cursor: 'pointer' }} />
+      </div>
+      <img src={littleLemonLogo} alt="" className='nav-logo' />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservation</li>
+        <li>Order</li>
+        <li>Login</li>
+      </ul>
+      <div className="cart">
+        <FaShoppingCart size={24} style={{ cursor: 'pointer' }} />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
